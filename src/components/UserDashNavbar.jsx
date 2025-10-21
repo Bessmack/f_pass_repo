1fadc7d53c1a126d4afaee8f81e9830a314bffef
Bessmack <jenwied4lif@gmail.com>
2025-10-22 01:08:21 +0300

    update dash navbar

--- src/components/UserDashNavbar.jsx
+++ src/components/UserDashNavbar.jsx
@@ -151,18 +151,6 @@ function UserDashNavbar() {
               </div>
             )}
           </div>
-
-          <button className="mobile-menu-toggle" onClick={toggleMenu}>
-            {isMenuOpen ? (
-              <svg viewBox="0 0 24 24" fill="none" stroke="currentColor">
-                <path d="M18 6L6 18M6 6l12 12" />
-              </svg>
-            ) : (
-              <svg viewBox="0 0 24 24" fill="none" stroke="currentColor">
-                <path d="M3 12h18M3 6h18M3 18h18" />
-              </svg>
-            )}
-          </button>
         </div>
       </div>
     </nav>
